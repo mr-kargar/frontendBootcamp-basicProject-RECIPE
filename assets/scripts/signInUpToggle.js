adb6260c6329a4eac8de33a1b262babab7fe3a27
Amin Kargar <98560519+mr-kargar@users.noreply.github.com>
2023-09-17 00:27:49 +0330

    Styling the forms and making the page responsive

--- assets/scripts/signInUpToggle.js
+++ assets/scripts/signInUpToggle.js
@@ -3,6 +3,8 @@ const signInForm = document.getElementById('signIn-form');
 const signInHeader = document.getElementById('signIn-header');
 const signUpForm = document.getElementById('signUp-form');
 const signUpHeader = document.getElementById('signUp-header');
+const toggleLoginSignIn = document.getElementById('toggle-login-signIn');
+const toggleLoginSignUp = document.getElementById('toggle-login-signUp');
 
 toggle.onchange = ()=>{
     if(toggle.checked){
@@ -10,11 +12,15 @@ toggle.onchange = ()=>{
         signInHeader.style.display = 'none';
         signUpForm.style.display = 'flex';
         signUpHeader.style.display = 'block';
+        toggleLoginSignIn.style.color = '#848A8D';
+        toggleLoginSignUp.style.color = '#000000';
     }else{
         signInForm.style.display = 'flex';
         signInHeader.style.display = 'block';
         signUpForm.style.display = 'none';
         signUpHeader.style.display = 'none';
+        toggleLoginSignUp.style.color = '#848A8D';
+        toggleLoginSignIn.style.color = '#000000';
     }
 
 }
