e2495bc26ef919902920fa4c2201c908bd4599ef
Amin Kargar <98560519+mr-kargar@users.noreply.github.com>
2023-09-20 20:45:19 +0330

    Add the ability to hide and display the password in the signUp form

--- assets/scripts/showpassword.js
+++ assets/scripts/showpassword.js
@@ -4,12 +4,47 @@ const signInPassword = document.getElementById("signInPassword");
 
 passwordShow.onclick = () => {
   signInPassword.type = "text";
-  passwordShow.style.display = 'none';
-  passwordHidden.style.display= 'inline-block';
-}
+  passwordShow.style.display = "none";
+  passwordHidden.style.display = "inline-block";
+};
 
 passwordHidden.onclick = () => {
-    signInPassword.type = "password";
-    passwordShow.style.display = 'inline-block';
-    passwordHidden.style.display= 'none';
-  }
+  signInPassword.type = "password";
+  passwordShow.style.display = "inline-block";
+  passwordHidden.style.display = "none";
+};
+
+const passwordSignUpShow = document.getElementById("password-signUp-show");
+const passwordSignUpHidden = document.getElementById("password-signUp-hidden");
+const signUpPassword = document.getElementById("signUpPassword");
+
+
+passwordSignUpShow.onclick = ()=>{
+  signUpPassword.type='text';
+  passwordSignUpShow.style.display = "none";
+  passwordSignUpHidden.style.display = "inline-block";
+}
+
+passwordSignUpHidden.onclick = ()=>{
+  signUpPassword.type='password';
+  passwordSignUpShow.style.display = "inline-block";
+  passwordSignUpHidden.style.display = "none";
+}
+
+
+const passwordConfirmSignUpShow = document.getElementById("passwordConfirm-signUp-show");
+const passwordConfirmSignUpHidden = document.getElementById("passwordConfirm-signUp-hidden");
+const confirmPassword = document.getElementById("confirmPassword");
+
+
+passwordConfirmSignUpShow.onclick = ()=>{
+  confirmPassword.type='text';
+  passwordConfirmSignUpShow.style.display = "none";
+  passwordConfirmSignUpHidden.style.display = "inline-block";
+}
+
+passwordConfirmSignUpHidden.onclick = ()=>{
+  confirmPassword.type='password';
+  passwordConfirmSignUpShow.style.display = "inline-block";
+  passwordConfirmSignUpHidden.style.display = "none";
+} 
